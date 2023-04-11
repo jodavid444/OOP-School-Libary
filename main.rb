5c5915a0e00397f99e676907b4c18188d376301c
Joseph David <jodavid444dave@gmail.com>
2023-04-12 00:34:12 +0100

    Update main.rb file

--- main.rb
+++ main.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'person'
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-CapitalizedPerson = CapitalizeDecorator.new(person)
-CapitalizedPerson.correct_name
-CapitalizedTrimmedPerson = TrimmerDecorator.new(CapitalizedPerson)
-CapitalizedTrimmedPerson.correct_name
 
-puts(person.correct_name)
-puts(CapitalizedPerson.correct_name)
-puts(CapitalizedTrimmedPerson.correct_name)
