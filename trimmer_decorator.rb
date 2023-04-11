2a847037fdbabc85237379cf9ab27ec59b782631
Joseph David <jodavid444dave@gmail.com>
2023-04-11 14:14:19 +0100

    Decorate person class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'decorator'
+
+# here is the top level documentation
+class TrimmerDecorator < Decorator
+  def correct_name
+    return super.slice(0..9) unless super.length <= 10
+
+    super
+  end
+end
