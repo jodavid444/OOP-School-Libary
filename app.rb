54287527821635bd7eb61e8e336038b5e6b5fa8f
Joseph David <jodavid444dave@gmail.com>
2023-04-17 22:21:01 +0100

    Refractor main.rb file

--- app.rb
+++ app.rb
@@ -5,25 +5,11 @@ require_relative 'rental'
 
 class App
   def initialize
-    @cmd = {
-      '1': 'List all books',
-      '2': 'List all people',
-      '3': 'Create a person',
-      '4': 'Create a book',
-      '5': 'Create a rental',
-      '6': 'List all rentals for a given person id',
-      '7': 'Exit'
-    }
+
 
     @books = []
 
     @people = []
-  end
-
-  def display_cmd
-    @cmd.each do |index, command|
-      puts "#{index} - #{command}"
-    end
   end
 
   def list_all_books
@@ -146,31 +132,5 @@ class App
         end
       end
     end
-  end
-
-  def choose_option(input)
-    case input
-    when '1' then list_all_books
-    when '2' then list_all_people
-    when '3' then create_person
-    when '4' then create_book
-    when '5' then create_rental
-    when '6' then rentals_of_person
-    end
-  end
-
-  def exit_option
-    input = gets.chomp
-    if input == '7'
-      exit
-    else
-      choose_option(input)
-    end
-  end
-
-  def run
-    puts 'Please choose an option to start'
-    display_cmd
-    exit_option
   end
 end
