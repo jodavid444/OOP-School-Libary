659eabd3bed33e40e704122477ef9b9ea14dc998
Joseph David <jodavid444dave@gmail.com>
2023-04-17 21:17:59 +0100

    Update main.rb file  as the define entry point

--- main.rb
+++ main.rb
@@ -1,14 +1,11 @@
-# frozen_string_literal: true
+require_relative 'app'
 
-require_relative 'person'
+def main
+  app = App.new
+  loop do
+    app.run
+    puts
+  end
+end
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-CapitalizedPerson = CapitalizeDecorator.new(person)
-CapitalizedPerson.correct_name
-CapitalizedTrimmedPerson = TrimmerDecorator.new(CapitalizedPerson)
-CapitalizedTrimmedPerson.correct_name
-
-puts(person.correct_name)
-puts(CapitalizedPerson.correct_name)
-puts(CapitalizedTrimmedPerson.correct_name)
+main
